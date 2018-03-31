6863dec00b9f377eda4258e604ee3ceb63264f39
Pagani Walter <paganiwalter@gmail.com>
2018-03-31 05:00:13 -0300

    quitando #

--- html/js/funciones.js
+++ html/js/funciones.js
@@ -10,20 +10,47 @@ $(document).ready(function(){
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault();
+        event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+        // Store hash
+        var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          // window.location.hash = hash;
+        });
     }  // End if
   });
 });
+
+// $(document).ready(function(){
+
+//      $('a[href*=#]').click(function() {
+
+//      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+//          && location.hostname == this.hostname) {
+
+//              var $target = $(this.hash);
+
+//              $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+//              if ($target.length) {
+
+//                  var targetOffset = $target.offset().top;
+
+//                  $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+//                  return false;
+
+//             }
+
+//        }
+
+//    });
+
+// });
